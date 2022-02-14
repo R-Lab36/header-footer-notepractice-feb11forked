21562153f71560758172e9e5650f1a613f351e33
Rachel <rmacleod520@gmail.com>
2022-02-14 11:12:43 -0700

    practise/troubles
    SO much node_modules :0

---
+++ src/conponants/Notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. Howmany programmers does it take to change a light bulb? A. None - It;s a harware problem"
+  },
+  {
+    Key: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo instructions: Lather. Rinse. Repeat."
+  },
+  {
+    Key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit their job? A. Because he didn't get arrays."
+  },
+  {
+    Key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "Whats the difference between hardware and software? You can hit your hardware with a hammer but you can only curse at your software."
+  }
+];
+
+export default notes;
